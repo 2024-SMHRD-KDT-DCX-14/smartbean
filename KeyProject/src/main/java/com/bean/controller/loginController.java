332088da761116a350c3254b8d3d2d6077338ca6
smhrd1 <smhrd1@smhrd>
2024-11-27 12:40:43 +0900

    커밋함

--- KeyProject/src/main/java/com/bean/controller/loginController.java
+++ KeyProject/src/main/java/com/bean/controller/loginController.java
@@ -17,51 +17,12 @@ public class loginController {
 	@Autowired
 	private MemberMapper mapper; 
 	
-	@RequestMapping("/join") //goMain 요청이 들어오면, goMain()을 실행하라***************
+	@RequestMapping("/login") //login 요청이 들어오면, login페이지 실행하라***************
 	public String go() {
-		// 1. 데이터 수집
-		// 2. 기능 실행
-		// 3. View 이동
-		// forward 이동시, 이동하고 싶은 "jsp파일의 이름"만 return하면된다.
-		return "join";
+		return "login";
 	}
+		
 
-	@RequestMapping("/join") //해당 요청이 들어오면, 이 메소드를 실행한다는 어노테이션(주석)!
-	public String join(Member member) {
-		// 1. 데이터 수집
-		// 메소드의 매개변수 자리에 변수를 선언해두면 자동으로 수집
-		// 변수명 == name
-		// 형변환도 자동으로 진행
-
-		// 2. 기능 실행
-		// >> DB에 회원정보 저장하기
-		int cnt = mapper.join(member);
-		
-		if(cnt >0) {
-			System.out.println("join t");
-		}else {
-			System.out.println("join f");
-		}
-		
-		// 3. View 이동
-		return "redirect:/join";
-		
-	}
-	
-	@RequestMapping("/login")
-	public String login(Member member, HttpSession session) {
-		// 1. 데이터 수집 -> 매개변수에 Member의 객체를 생성한다.
-		// 2. 기능 실행
-		Member result = mapper.login(member);
-		if(result== null) {
-			System.out.println("login f");
-		}else {
-			System.out.println("login t");
-			session.setAttribute("user", result);
-		}
-		// 3. View 이동
-		return "redirect:/join";
-	}
 	
 
 }
