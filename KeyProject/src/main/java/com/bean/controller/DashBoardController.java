8cb932177825b9be9c555f656f0b7d1b0a7da1ef
smhrd1 <smhrd1@smhrd>
2024-11-30 18:04:17 +0900

    메인 한거까지 커밋

--- KeyProject/src/main/java/com/bean/controller/DashBoardController.java
+++ KeyProject/src/main/java/com/bean/controller/DashBoardController.java
@@ -6,11 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DashBoardController {
 	
-
+	// 메뉴별 대시보드로 이동
 	@RequestMapping("/menudashboard") 
 	 public void gomenudashboard() {
 	 	}
 	
+	// 기간별 대시보드로 이동
 	@RequestMapping("/perioddashboard")
 	public void goperioddashboard() {
 		
